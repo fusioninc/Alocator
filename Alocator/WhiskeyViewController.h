8a119fc6b9d910ae340f24516125023b89c28eb2
Jason Kuang <jason.jkuang@gmail.com>
2015-06-13 00:49:49 -0700

    adding whiskeyViewController class

---
+++ Alocator/WhiskeyViewController.h
@@ -0,0 +1,13 @@
+//
+//  WhiskeyViewController.h
+//  Alocator
+//
+//  Created by Fusion on 6/12/15.
+//  Copyright (c) 2015 Jason Kuang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface WhiskeyViewController : ViewController
+
+@end
